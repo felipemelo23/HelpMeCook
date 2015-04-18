43a02cb55159594006bcb384f93e7d2a8e98c349
felipemelo23 <felipemelosoares23@gmail.com>
2015-04-18 09:25:40 -0300

    Exemplo de CardView
    Eu criei uma activiy só para testar os CardViews e mudei a activity
    laucher para ser ela. MainActivity2Activity, MyAdapter,
    activity_main_activity2.xml, my_text_view.xml, menu_main_activity2.xml
    são os arquivos que eu fiz/modifiquei para fazer o CardView.
    Por fim, como a gente não vai usar o material design, acho que não
    usaremos os CardViews mesmo.

---
+++ HelpMeCook/app/src/main/java/br/com/helpmecook/MainActivity2Activity.java
@@ -0,0 +1,68 @@
+package br.com.helpmecook;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+
+public class MainActivity2Activity extends ActionBarActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        ArrayList<String> myDataset = new ArrayList<String>();
+
+        myDataset.add("Felipe");
+        myDataset.add("Melo");
+        myDataset.add("Soares");
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new GridLayoutManager(this,2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_activity2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
