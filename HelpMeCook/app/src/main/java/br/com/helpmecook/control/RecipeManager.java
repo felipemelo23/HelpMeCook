5ff938d5c55c8d87894fbee1a6332756361b61c7
felipemelo23 <felipemelosoares23@gmail.com>
2015-04-30 08:15:38 -0300

    RecipeManager e ConnectionAccessor

---
+++ HelpMeCook/app/src/main/java/br/com/helpmecook/control/RecipeManager.java
@@ -0,0 +1,50 @@
+package br.com.helpmecook.control;
+
+import java.util.List;
+
+/**
+ * Created by Felipe on 30/04/2015.
+ */
+public class RecipeManager {
+    private ConnectionAccessor accessor;
+
+    public Recipe getRecipeById() {
+        return null;
+    }
+
+    public List<AbstractRecipe> getAbstractRecipes(List<Integer>) {
+        return null;
+    }
+
+    public List<Integer> getResultByIngredientLists(List<Ingredient> wanted, List<Ingredient> unwanted) {
+        return null;
+    }
+
+    public List<Integer> getPlusByIngredientLists(List<Ingredient> wanted, List<Ingredient> unwanted) {
+        return null;
+    }
+
+    public List<Integer> getResultByRecipeName(String name) {
+        return null;
+    }
+
+    public List<Integer> getRecentRecipes() {
+        return null;
+    }
+
+    public List<Integer> getPopularRecipes() {
+        return null;
+    }
+
+    public Boolean registerRecipe(Recipe recipe) {
+        return false;
+    }
+
+    public Boolean classifyRecipe(int id, float taste, float difficult) {
+        return false;
+    }
+
+    public Boolean syncAll() {
+        return false;
+    }
+}
