2d708320a8c7a0e85995dcb2279c1daeef1b4306
felipemelo23 <felipemelosoares23@gmail.com>
2015-05-01 12:24:03 -0300

    Alterações na Main

--- HelpMeCook/app/src/main/java/br/com/helpmecook/MainActivity.java
+++ HelpMeCook/app/src/main/java/br/com/helpmecook/MainActivity.java
@@ -1,6 +1,5 @@
 package br.com.helpmecook;
 
-<<<<<<< HEAD
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
@@ -16,10 +15,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-
+import br.com.helpmecook.view.adapter.NavDrawerListAdapter;
 import java.util.ArrayList;
-
-import br.com.helpmecook.adapter.NavDrawerListAdapter;
 import br.com.helpmecook.model.NavDrawerItem;
 
 public class MainActivity extends ActionBarActivity {
@@ -44,25 +41,6 @@ public class MainActivity extends ActionBarActivity {
     private ArrayList<NavDrawerItem> navDrawerItems;
     // adapter da navigation drawer
     private NavDrawerListAdapter adapter;
-=======
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.widget.DrawerLayout;
-
-import br.com.helpmecook.view.fragment.PlaceholderFragment;
-
-public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
-
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    private CharSequence mTitle;
-    private String[] navMenuTitles;
->>>>>>> 229ff594554c2008ce8c6cbf3d094595e0641231
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -233,43 +211,5 @@ public class MainActivity extends ActionBarActivity
         editor.putInt(PREFS_POSICAO, posicao);
         editor.commit();
     }
-
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
-<<<<<<< HEAD
-    }*/
-
 }
-=======
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-}
->>>>>>> 229ff594554c2008ce8c6cbf3d094595e0641231
