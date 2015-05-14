098a9212c7f8f8811ab8d95814056beef30d01e0
ThaeRen <thaeren7@gmail.com>
2015-05-14 14:55:57 -0300

    Mudanças nos xmls da nav drawer

--- HelpMeCook/app/src/main/java/br/com/helpmecook/view/fragment/NavigationDrawerFragment.java
+++ HelpMeCook/app/src/main/java/br/com/helpmecook/view/fragment/NavigationDrawerFragment.java
@@ -104,6 +104,7 @@ public class NavigationDrawerFragment extends Fragment {
                              Bundle savedInstanceState) {
         mDrawerListView = (ListView) inflater.inflate(
                 R.layout.fragment_navigation_drawer, container, false);
+
         mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
