874f8c22ecd611551dfd8d9cfd6e88f1d2a66314
ThaeRen <thaeren7@gmail.com>
2015-05-20 10:41:33 -0300

    Listeners da classe RecipeActivity

--- HelpMeCook/app/src/main/java/br/com/helpmecook/view/activity/RecipeActivity.java
+++ HelpMeCook/app/src/main/java/br/com/helpmecook/view/activity/RecipeActivity.java
@@ -1,9 +1,12 @@
 package br.com.helpmecook.view.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -52,9 +55,29 @@ public class RecipeActivity extends Activity {
         // por enquanto as receitas nao tem imagens
 
         addCookBook = (ImageButton) findViewById(R.id.add_cookbook);
+        // Se a receita já estiver no cookbook, o botão de adicionar vira de remover
+        if (Manager.isOnCookbook(recipe, RecipeActivity.this)) {
+            addCookBook.setImageDrawable(getResources().getDrawable(R.drawable.cookbook_minus));
+        }
+        addCookBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Manager.isOnCookbook(recipe, RecipeActivity.this)) {
+                    removeFromCookbook();
+                } else {
+                    addToCookbook();
+                }
+            }
+        });
 
         rbTaste = (RatingBar) findViewById(R.id.rb_taste);
         rbTaste.setRating(recipe.getTaste());
+        rbTaste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                classifyTaste();
+            }
+        });
 
         rbDifficulty = (RatingBar) findViewById(R.id.rb_difficulty);
         rbDifficulty.setRating(recipe.getDifficulty());
@@ -67,19 +90,89 @@ public class RecipeActivity extends Activity {
     }
 
     public boolean addToCookbook() {
-        return Manager.addToCookbook(recipe, this);
+        boolean result = Manager.addToCookbook(recipe, RecipeActivity.this);
+        if (result) {
+            addCookBook.setImageDrawable(getResources().getDrawable(R.drawable.cookbook_minus));
+            return result;
+        } else {
+            return result;
+        }
     }
 
     public boolean removeFromCookbook() {
-        return Manager.removeFromCookbook(recipe, this);
+        boolean result = Manager.removeFromCookbook(recipe, RecipeActivity.this);
+        if (result) {
+            addCookBook.setImageDrawable(RecipeActivity.this.getResources().getDrawable(R.drawable.cookbook_plus));
+            return result;
+        } else {
+            return result;
+        }
     }
 
-    public boolean classifyTaste(float taste) {
-        return Manager.classifyTaste(recipeId, taste);
+    public boolean classifyTaste() {
+        final boolean result;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(RecipeActivity.this);
+
+        View view = getLayoutInflater().inflate(R.layout.dialog_classify, null);
+        builder.setView(view);
+        final RatingBar ratingBar = (RatingBar) view.findViewById(R.id.rb_classify);
+
+
+        builder.setTitle(RecipeActivity.this.getResources().getString(R.string.classify_taste));
+        builder.setNegativeButton(RecipeActivity.this.getResources().getString(R.string.cancel),
+                new android.content.DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int arg1) {
+                result = false;
+                dialog.cancel();
+            }
+        });
+        builder.setPositiveButton("Ok", new android.content.DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int arg1) {
+                result = Manager.classifyTaste(recipeId, ratingBar.getRating());
+                dialog.dismiss();
+            }
+        });
+
+       builder.create();
+        return result;
     }
 
-    public boolean classifyDifficulty(float difficulty) {
-        return Manager.classifyDifficulty(recipeId, difficulty);
+    public boolean classifyDifficulty() {
+        final boolean result;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(RecipeActivity.this);
+
+        View view = getLayoutInflater().inflate(R.layout.dialog_classify, null);
+        builder.setView(view);
+        final RatingBar ratingBar = (RatingBar) view.findViewById(R.id.rb_classify);
+
+
+        builder.setTitle(RecipeActivity.this.getResources().getString(R.string.classify_difficulty));
+        builder.setNegativeButton(RecipeActivity.this.getResources().getString(R.string.cancel),
+                new android.content.DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int arg1) {
+                        result = false;
+                        dialog.cancel();
+                    }
+                });
+        builder.setPositiveButton("Ok", new android.content.DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int arg1) {
+                result = Manager.classifyDifficulty(recipeId, ratingBar.getRating());
+                dialog.dismiss();
+            }
+        });
+
+        builder.create();
+        return result;
     }
 
     @Override
