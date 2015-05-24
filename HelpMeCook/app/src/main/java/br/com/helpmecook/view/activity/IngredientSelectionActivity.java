274e45bdaeb267c005d8d9cb3e3b988ebe2533c8
MarianaFL <mflgeb@gmail.com>
2015-05-23 21:58:43 -0300

    classe de IngredientSelectionActivity pronta para testes

--- HelpMeCook/app/src/main/java/br/com/helpmecook/view/activity/IngredientSelectionActivity.java
+++ HelpMeCook/app/src/main/java/br/com/helpmecook/view/activity/IngredientSelectionActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -14,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.helpmecook.R;
+import br.com.helpmecook.control.Manager;
 import br.com.helpmecook.model.Ingredient;
 import br.com.helpmecook.view.adapter.IngredientSelectionAdapter;
 
@@ -24,8 +28,8 @@ import br.com.helpmecook.view.adapter.IngredientSelectionAdapter;
 public class IngredientSelectionActivity extends ActionBarActivity {
 
     private ListView lvList;
-    private int wantedIngredients[];
-    private int unwantedIngredients[];
+    private ArrayList<Long> wantedIngredients;
+    private ArrayList<Long> unwantedIngredients;
     private List<Ingredient> allIngredients;
     IngredientSelectionList ingredient_data[];
     public List<Integer> clicked;
@@ -62,9 +66,6 @@ public class IngredientSelectionActivity extends ActionBarActivity {
 
         lvList = (ListView)findViewById(R.id.lvListIngredient);
 
-//        View header = (View)getLayoutInflater().inflate(R.layout.ingredient_selection_activity, null);
-//        lvList.addHeaderView(header);
-
         lvList.setAdapter(ingredientSelectionAdapter);
 
         lvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -95,7 +96,7 @@ public class IngredientSelectionActivity extends ActionBarActivity {
             executeSearch();
         }
         else{
-            wantedIngredients = new int[allIngredients.size()];
+            wantedIngredients = new long[allIngredients.size()];
             Intent intent = getIntent();
 
             for (int i=0; i<clicked.size(); i++) {
@@ -110,16 +111,23 @@ public class IngredientSelectionActivity extends ActionBarActivity {
     }
 
     private void executeSearch(){
-        wantedIngredients = new int[allIngredients.size()];
-        unwantedIngredients = new int[allIngredients.size()- wantedIngredients.length];
+        wantedIngredients = new ArrayList<Long>(allIngredients.size());
+        unwantedIngredients = new ArrayList<Long>(allIngredients.size()-wantedIngredients.size());
         Intent intent = new Intent(getApplicationContext(),IngredientSearchResultActivity.class);
 
         for (int i=0; i<clicked.size(); i++){
             if (clicked.get(i)==1){
-                wantedIngredients[i] = allIngredients.get(i).getId();
+                wantedIngredients.set(i,allIngredients.get(i).getId());
             }
             if (clicked.get(i)==2){
-                unwantedIngredients[i] = allIngredients.get(i).getId();
+                unwantedIngredients.set(i,allIngredients.get(i).getId());
+            }
+        }
+        if(wantedIngredients.isEmpty()) {
+            for (int i=0; i<clicked.size(); i++){
+                if (clicked.get(i) != 2){
+                    wantedIngredients.set(i,allIngredients.get(i).getId());
+                }
             }
         }
 
@@ -128,7 +136,39 @@ public class IngredientSelectionActivity extends ActionBarActivity {
         startActivity(intent);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_ingredient_selection_activity, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_ok:
+                //abaixo o conteúdo que estava na finada função buttonClick
+                if (origin == MainActivity.MAIN) {
+                    executeSearch();
+                }
+                else{
+                    wantedIngredients = new ArrayList<Long>(allIngredients.size());
+                    Intent intent = getIntent();
+
+                    for (int i=0; i<clicked.size(); i++) {
+                        if (clicked.get(i) == 1) {
+                            wantedIngredients.set(i,allIngredients.get(i).getId());
+                        }
+                    }
+                    intent.putExtra(WANTED_INGREDIENTS, wantedIngredients);
+                    setResult(MainActivity.RESULT_OK, intent);
+                    finish();
+                }
+                return false;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 
 }
