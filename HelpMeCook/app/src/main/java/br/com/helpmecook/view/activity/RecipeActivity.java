faca0c168f5b2228b6c68a3cecd2712cff2300d0
felipemelo23 <felipemelosoares23@gmail.com>
2015-05-27 21:26:49 -0300

    Alterações na RecipeActivity

--- HelpMeCook/app/src/main/java/br/com/helpmecook/view/activity/RecipeActivity.java
+++ HelpMeCook/app/src/main/java/br/com/helpmecook/view/activity/RecipeActivity.java
@@ -6,8 +6,10 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -80,9 +82,11 @@ public class RecipeActivity extends ActionBarActivity {
 
         rbTaste = (RatingBar) findViewById(R.id.rb_taste);
         rbTaste.setRating(recipe.getTaste());
+        rbTaste.setClickable(true);
         rbTaste.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i("RecipeActivity", "Classify Taste Pressed");
                 classifyTaste();
             }
         });
@@ -92,6 +96,7 @@ public class RecipeActivity extends ActionBarActivity {
         rbDifficulty.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i("RecipeActivity", "Classify Difficulty Pressed");
                 classifyDifficulty();
             }
         });
