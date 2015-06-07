e9ea5003fa2c5668779deec145db8bb37cc0c9d1
AndreChibi <andrenoz@gmail.com>
2015-06-06 22:05:11 -0300

    GoogleMaps

--- HelpMeCook/app/src/main/java/br/com/helpmecook/view/fragment/MapFragment.java
+++ HelpMeCook/app/src/main/java/br/com/helpmecook/view/fragment/MapFragment.java
@@ -1,6 +1,10 @@
 package br.com.helpmecook.view.fragment;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +12,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -19,15 +30,18 @@ import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.InputStream;
+import java.security.Provider;
 import java.util.ArrayList;
 
 import br.com.helpmecook.R;
 import br.com.helpmecook.model.GooglePlace;
+import br.com.helpmecook.view.activity.MainActivity;
 
 /**
  * Created by Thais on 26/05/2015.
  */
 public class MapFragment extends Fragment {
+    private GoogleMap googleMap;
 
     ArrayList<GooglePlace> places;
 
@@ -43,9 +57,45 @@ public class MapFragment extends Fragment {
         View fragmentView = inflater.inflate(R.layout.fragment_map,
                 container, false);
 
+//        setUpMapIfNeeded();
+
         new GooglePlaceTask().execute();
 
         return fragmentView;
+    }
+//    private void setUpMapIfNeeded() {
+//        // Do a null check to confirm that we have not already instantiated the map.
+//        if (googleMap == null) {
+//            // Try to obtain the map from the SupportMapFragment.
+//            googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+//            // Check if we were successful in obtaining the map.
+//            if (googleMap != null) {
+//                setUpMap();
+//            }
+//        }
+//    }
+
+    private void setUpMap(){
+        googleMap.setMyLocationEnabled(true);
+
+
+        LocationManager locationManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+        double latitude = myLocation.getLatitude();
+        double longitude = myLocation.getLongitude();
+        LatLng latLng = new LatLng(latitude, longitude);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(18));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("tu"));
+
     }
 
     public class GooglePlaceTask extends AsyncTask {
@@ -53,7 +103,8 @@ public class MapFragment extends Fragment {
 
         @Override
         protected Object doInBackground(Object[] params) {
-            temp = fazerChamada("https://maps.googleapis.com/maps/api/place/search/json?location=" + LAT + "," + LON + "&radius=100&sensor=true&key=" + GOOGLE_KEY);
+            temp = fazerChamada("https://maps.googleapis.com/maps/api/place/search/json?location=" + LAT + "," + LON + "&radius=100&sensor=true&key=" + GOOGLE_KEY +
+                    "&types=restaurant&types=grocery_or_supermarket");
             System.out.println("Acessando: " + "https://maps.googleapis.com/maps/api/place/search/json?location=" + LAT + "," + LON + "&radius=100&sensor=true&key=" + GOOGLE_KEY);
             return null;
         }
@@ -61,7 +112,7 @@ public class MapFragment extends Fragment {
         @Override
         protected void onPostExecute(Object o) {
             if (temp == null) {
-                // Chamada deu errado. Cota atingida? Conexão perdida?
+                // Chamada deu errado. Cota atingida? Conexï¿½o perdida?
             }
             places = parseGoogleParse(temp);
 
