457f0f8a49e7265e3711518a3a1cb768a3f6cb0f
ThaeRen <thaeren7@gmail.com>
2015-06-18 10:59:33 -0300

    Apagando a connection

--- HelpMeCook/app/src/main/java/br/com/helpmecook/control/ConnectionAccessor.java
+++ HelpMeCook/app/src/main/java/br/com/helpmecook/control/ConnectionAccessor.java
@@ -1,43 +1,19 @@
 package br.com.helpmecook.control;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import android.util.Base64;
 import android.util.Log;
 import android.util.Pair;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.conn.HttpHostConnectException;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.ByteArrayBuffer;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.jar.Attributes;
 
 import br.com.helpmecook.connection.JsonParser;
-import br.com.helpmecook.connection.UploadRecipe;
 import br.com.helpmecook.model.AbstractRecipe;
 import br.com.helpmecook.model.Ingredient;
 import br.com.helpmecook.model.Recipe;
